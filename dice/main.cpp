31f56aa84b115f3d583b08bf8a9b0b6e3fd93a65
Omar Sharaf <omar.sharaf@live.ca>
2021-03-28 22:25:51 -0400

    change

--- dice/main.cpp
+++ dice/main.cpp
@@ -3,54 +3,40 @@ using namespace std;
 int main() {
   int roll1,roll2,sum,points;
     string name;
-    char quit;
     cout<<"we'll be playing a dice game";
     cout<<"\n-------\n|     |\n|  o  |\n|     |\n-------\n-------\n| o o |\n| o o |\n| o o |\n-------\n";
-    points=500;
+    
   cout<<"what's your name? ";
   cin>>name;
-  while ((points <=  900)or(points >= 0)){
+  while ((points <=  900)or(points >=-1)){
   cout << "input first roll "<<name<<" ";
   cin>>roll1;
   cout << "input second roll "<<name<<" ";
   cin>>roll2;
-  cout<<"do you want to quit(one letter y or n) ";
-  cin>>quit;
   sum=roll1+roll2;
-  if (quit == 'y'){
-  cout<<"you've quit\n";
-  cout<<"bye\n";
-  break;
-}
-else if(quit == 'n'){
-cout<<"you have not quit\n"; 
-}
+  cout << sum<<"\n";
+
+
 if ((sum == 11) or (sum == 7)){
-cout<<"winner\n";
 points=points+100;
 cout<<points<<"\n";
 }
+else if ((roll1>6)or(roll1<1)or(roll2>6)or(roll2<1)){
+  cout<<"invalid\n";
+  points=points-100;
+  cout<<points<<"\n";
+}
 else if ((roll1=5)and roll1==roll2){
-cout<<"winner\n";
 points=points+200;
 cout<<points<<"\n";
 }
-else {
-  cout<<"loser\n";
-  points=points-25;
+else if ((roll1>6)or(roll1<1)or(roll2>6)or((roll2=5)and(roll1=roll2))){
+  cout<<"invalid\n";
+  points=points-200;
   cout<<points<<"\n";
 }
-if (points >= 999  ){
-  cout<<"you won with "<<points<<" points"<<"\n";
-  cout<<"░░░░░░░░░░░░░░░░░░░░░░█████████\n░░███████░░░░░░░░░░███▒▒▒▒▒▒▒▒███\n░░█▒▒▒▒▒▒█░░░░░░░███▒▒▒▒▒▒▒▒▒▒▒▒▒███\n░░░█▒▒▒▒▒▒█░░░░██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██\n░░░░█▒▒▒▒▒█░░░██▒▒▒▒▒██▒▒▒▒▒▒██▒▒▒▒▒███\n░░░░░█▒▒▒█░░░█▒▒▒▒▒▒████▒▒▒▒████▒▒▒▒▒▒██\n░░░█████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██\n░░░█▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒██\n░██▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒██▒▒▒▒▒▒▒▒▒▒██▒▒▒▒██\n██▒▒▒███████████▒▒▒▒▒██▒▒▒▒▒▒▒▒██▒▒▒▒▒██\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒████████▒▒▒▒▒▒▒██\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██\n░█▒▒▒███████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██\n░██▒▒▒▒▒▒▒▒▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█\n░░████████████░░░█████████████████\n";
-  cout<<"bye\n";
-  break;
   }
-  else if (points <= 0){
-  cout<<"you lost with "<<points<<" points"<<"\n";
-  cout<<"¯|_(ツ)_/¯\n";
-  cout<<"bye\n";
-  break;
+if (points == 1000){
+  cout<<"you won "<<points<<"\n";
 }
-  }
 }
